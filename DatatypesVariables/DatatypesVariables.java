87a88d06d69ae2b8961a2c1cd89f87a7ec67ba59
Joel Prashanth <142143829+joel-prashanth@users.noreply.github.com>
2025-04-24 11:30:34 +0530

    Finish Datatypes and Variables

---
+++ DatatypesVariables/DatatypesVariables.java
@@ -0,0 +1,15 @@
+package DatatypesVariables;
+
+public class DatatypesVariables {
+
+    int playerScore_; /*Declare */
+    int playerScore = 10; /*Initialize*/
+
+    char choice = '#';
+
+    String name = "Joel";
+
+    boolean isAbsent = false;
+
+    double BMI = 26.4;
+}
