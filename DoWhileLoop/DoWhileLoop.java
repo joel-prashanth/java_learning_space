09135278ad971d01edd118af100f30de91a791db
Joel Prashanth <142143829+joel-prashanth@users.noreply.github.com>
2025-04-24 15:32:09 +0530

    Finish Do While Loops

---
+++ DoWhileLoop/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package DoWhileLoop;
+
+public class DoWhileLoop {
+    public static void main(String[] args){
+        int count = 23;
+
+        do {
+            System.out.println("This is the loop.");
+            count = count - 10;
+        } while (count < 5);
+    }
+}
