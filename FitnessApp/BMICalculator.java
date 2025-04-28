867b81cf29ba2987ccfee7cbbaee31ed528e4f26
Joel Prashanth <142143829+joel-prashanth@users.noreply.github.com>
2025-04-28 20:01:50 +0530

    Finish grouping Methods

---
+++ FitnessApp/BMICalculator.java
@@ -0,0 +1,17 @@
+package FitnessApp;
+
+public class BMICalculator {
+
+    public double calculateBmiImperial(double weightInPounds, double heightInInches) {
+
+        return (weightInPounds / (heightInInches * heightInInches)) * 703;
+
+    }
+
+    public double calculateBmiMetric(double weightInKilos, double heightInMeters) {
+
+        return weightInKilos / (heightInMeters * heightInMeters);
+
+    }
+
+}
